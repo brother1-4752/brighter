4ba44b8914b8bb8f34e22947df1a2cc8d625e0bb
brother1-4752 <guddlf4752@gmail.com>
2023-08-28 15:22:38 +0900

    style: 글로벌 스타일 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,15 @@
+import GlobalStyle from '@/styles/GlobalStyle'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Brighter</title>
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
